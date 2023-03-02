78ff2406492c13c4acee73b465a76759fc3c3611
thisisaaronland <devnull@localhost>
2023-03-02 08:36:21 -0800

    notes to self: colours, wtf

--- cmd/convert/main.go
+++ cmd/convert/main.go
@@ -1,5 +1,10 @@
 package main
 
+// pdf (preview app):  #E8BCE3
+// png (preview app): #E1BEE2
+// pdf (moz): #e8bde4
+// png: #e8bde4
+
 import (
 	"context"
 	"flag"
@@ -7,7 +12,8 @@ import (
 	"image"
 	"image/color"
 	"image/draw"
-	"image/jpeg"
+	_ "image/jpeg"
+	"image/png"	
 	"log"
 	"os"
 	"path/filepath"
@@ -63,19 +69,23 @@ func main() {
 			log.Fatalf("Failed to derive images for %s, %v", path, err)
 		}
 
+		/*
 		jpeg_opts := &jpeg.Options{
 			Quality: 100,
 		}
-
+		*/
+		
 		for idx, im := range images {
 
 			i := idx + 1
 
 			jpeg_fname := strings.Replace(fname, ext, "", 1)
-			jpeg_fname = fmt.Sprintf("%s-%03d.jpg", jpeg_fname, i)
+			// jpeg_fname = fmt.Sprintf("%s-%03d.jpg", jpeg_fname, i)
+			jpeg_fname = fmt.Sprintf("%s-%03d.png", jpeg_fname, i)			
 			jpeg_path := filepath.Join(root, jpeg_fname)
 
-			temp_wr, err := os.CreateTemp("", "freeform.*.jpg")
+			// temp_wr, err := os.CreateTemp("", "freeform.*.jpg")
+			temp_wr, err := os.CreateTemp("", "freeform.*.png")			
 
 			if err != nil {
 				log.Fatalf("Failed to create temp file for %s, %v", path, err)
@@ -93,8 +103,9 @@ func main() {
 			draw.Draw(dst, dst.Bounds(), image.NewUniform(backgroundColor), image.Point{}, draw.Src)
 			draw.Draw(dst, dst.Bounds(), im, im.Bounds().Min, draw.Over)
 			
+			err = png.Encode(temp_wr, dst)
 			
-			err = jpeg.Encode(temp_wr, dst, jpeg_opts)
+			// err = jpeg.Encode(temp_wr, dst, jpeg_opts)
 
 			if err != nil {
 				log.Fatalf("Failed to write JPEG for %s, %v", jpeg_path, err)
@@ -106,6 +117,11 @@ func main() {
 				log.Fatalf("Failed to close %s, %v", jpeg_path, err)
 			}
 
+			os.Rename(temp_wr.Name(), jpeg_path)
+			log.Println(temp_wr.Name())
+			log.Println(jpeg_path)
+			continue
+			
 			jpeg_r, err := os.Open(temp_wr.Name())
 
 			if err != nil {
